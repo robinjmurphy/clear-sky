c14a561cf90e7004767fe19a32c5a44ecc9653dc
Robin Murphy <robin@robinmurphy.co.uk>
2014-07-05 12:46:47 +0100

    Add Heroku compatability

--- project/build.scala
+++ project/build.scala
@@ -15,7 +15,7 @@ object ClearSkyBuild extends Build {
   lazy val project = Project (
     "clear-sky",
     file("."),
-    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+    settings = seq(com.typesafe.sbt.SbtStartScript.startScriptForClassesSettings: _*) ++ Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
@@ -30,8 +30,8 @@ object ClearSkyBuild extends Build {
         "net.databinder.dispatch" %% "dispatch-core" % "0.11.0",
         "commons-io" % "commons-io" % "2.3",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,4 +2,4 @@ addSbtPlugin("com.mojolly.scalate" % "xsbt-scalate-generator" % "0.4.2")
 
 addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.2")
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-start-script" % "0.10.0")
